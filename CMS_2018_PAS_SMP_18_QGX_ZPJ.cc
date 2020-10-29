0dfe6c4259871e197c7401e6d842d5b18d7e4b2c
ahinzmann <hinzmann@cern.ch>
2020-10-29 17:00:44 +0100

    Update CMS_2018_PAS_SMP_18_QGX_ZPJ.cc

--- CMS_2018_PAS_SMP_18_QGX_ZPJ.cc
+++ CMS_2018_PAS_SMP_18_QGX_ZPJ.cc
@@ -31,7 +31,7 @@ namespace Rivet {
 class Angularity : public FunctionOfPseudoJet<double>{
 public:
   /// ctor
-  Angularity(double alpha, double jet_radius, double kappa=1.0, bool isCharged=false, Selector constitCut=SelectorPtMin(0.)) : _alpha(alpha), _radius(jet_radius), _kappa(kappa), _isCharged(isCharged), _constitCut(constitCut) {}
+  Angularity(double alpha, double jet_radius, double kappa=1.0, Selector constitCut=SelectorPtMin(0.)) : _alpha(alpha), _radius(jet_radius), _kappa(kappa), _constitCut(constitCut) {}
 
   /// description
   std::string description() const{
@@ -52,7 +52,6 @@ public:
     double numerator = 0.0, denominator = 0.0;
     unsigned int num = 0;
     for (const auto &c : constits){
-      if ((_isCharged) && (!c.user_index())) continue;
       if (!_constitCut.pass(c)) continue;
       double pt = c.pt();
       // Note: better compute (dist^2)^(alpha/2) to avoid an extra square root
@@ -61,7 +60,7 @@ public:
       num += 1;
     }
     if (denominator == 0) return -1;
-    else if ((num == 1) && (_kappa==0) && (_alpha==0)) return 1; // e.g. multiplicity variable
+    else if ((num == 1) && (_kappa==0) && (_alpha==0)) return 1; // multiplicity variable
     else if (num == 1) return 0;
     // the formula is only correct for the the typical angularities which satisfy either kappa==1 or alpha==0.
     else return numerator/(pow(denominator, _kappa)*pow(_radius, _alpha));
@@ -76,7 +75,6 @@ protected:
   }
 
   double _alpha, _radius, _kappa;
-  bool _isCharged;
   Selector _constitCut;
 };
 
@@ -298,14 +296,22 @@ protected:
 
         // UNGROOMED VERSION
         // -------------------------------------------------------------------
+        vector<PseudoJet> chargedParticles;
+        for (uint iC=0; iC<jet1.constituents().size(); iC++)
+          if (jet1.constituents()[iC].user_index())
+            chargedParticles.push_back(jet1.constituents()[iC]);
+        vector<PseudoJet> chargedJets = jet_def(chargedParticles);
 
         // Fill hists for each lambda variable
         for (uint lambdaInd=0; lambdaInd < _lambdaVars.size(); lambdaInd++) {
           const LambdaVar & thisLambdaVar = _lambdaVars[lambdaInd];
-          Angularity angularity(thisLambdaVar.beta, jetRadius, thisLambdaVar.kappa, thisLambdaVar.isCharged, thisLambdaVar.constitCut);
-          float val = angularity(jet1);
+          Angularity angularity(thisLambdaVar.beta, jetRadius, thisLambdaVar.kappa, thisLambdaVar.constitCut);
+          float val = -1;
+          if (thisLambdaVar.isCharged)
+            val = (chargedJets.size()>0) ? angularity(chargedJets[0]) : -1;
+          else
+            val = angularity(jet1);
           if (val<0) continue;
-
           _h_zpj[radiusInd][lambdaInd][ptBinInd]->fill(val, weight);
         }
 
@@ -317,14 +323,20 @@ protected:
         //fastjet::contrib::ModifiedMassDropTagger mmdt(0.1); mmdt.set_grooming_mode(); mmdt.set_reclustering(false);
         //Recluster ca_cluster(JetDefinition(cambridge_algorithm, JetDefinition::max_allowable_R));
         //PseudoJet groomedJet = mmdt(ca_cluster(jetItr));
+        PseudoJet groomedJetCharged;
+        if (chargedJets.size()>0)
+          groomedJetCharged= sd(chargedJets[0]);
 
         // Fill hists for each lambda variable
         for (uint lambdaInd=0; lambdaInd < _lambdaVars.size(); lambdaInd++) {
           const LambdaVar & thisLambdaVar = _lambdaVars[lambdaInd];
-          Angularity angularity(thisLambdaVar.beta, jetRadius, thisLambdaVar.kappa, thisLambdaVar.isCharged, thisLambdaVar.constitCut);
-          float val = angularity(groomedJet);
+          Angularity angularity(thisLambdaVar.beta, jetRadius, thisLambdaVar.kappa, thisLambdaVar.constitCut);
+          float val = -1;
+          if (thisLambdaVar.isCharged)
+           val = (chargedJets.size()>0) ? angularity(groomedJetCharged) : -1;
+          else
+           val = angularity(groomedJet);
           if (val<0) continue;
-
           _h_zpj_groomed[radiusInd][lambdaInd][ptBinInd]->fill(val, weight);
         }
       } // end loop over jet radii
